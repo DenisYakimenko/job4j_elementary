18ec3dc2cc224f6013a1dd29a94f57ede23147b4
Denis <dyakimenko@mail.ru>
2024-09-06 17:01:20 +0300

    изменение файлов Multiple

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -5,7 +5,7 @@ public class Multiple {
     public static void main(String[] args) {
 
         for (int i = 2; i < 10; i++) {
-            System.out.println("1 * " + i + " = " + 1 * i);
+            System.out.println("1 * " + i + " = " + i);
 
 
         }
