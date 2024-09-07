edc01fdd630d8f2a2ffdf071841a79dcb76fc7ad
Denis <dyakimenko@mail.ru>
2024-09-07 18:43:15 +0300

    Исправление пробелов.

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,29 +3,21 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second) + multiply(first, second);
     }
 
-
     public static double sumOfDifferenceAndDivision(double first, double second) {
-
         return difference(first, second) + division(first, second);
     }
 
     public static double sumOfAllOperations(double first, double second) {
-
         return sum(first, second) + difference(first, second) + division(first, second) + multiply(first, second);
-
     }
-
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 2));
         System.out.println("Результат расчета sumOfDifferenceAndDivision: " + sumOfDifferenceAndDivision(12, 5));
         System.out.println("Результат расчета sumOfAllOperations: " + sumOfAllOperations(8, 4));
     }
-
-
 }
--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -1,7 +1,6 @@
 package ru.job4j.math;
 
 public class MathFunction {
-
     public static double sum(double first, double second) {
         return first + second;
     }
@@ -10,11 +9,9 @@ public class MathFunction {
         return a - b;
     }
 
-
     public static double division(double first, double second) {
         return first / second;
     }
-
 
     public static double multiply(double first, double second) {
         return first * second;
