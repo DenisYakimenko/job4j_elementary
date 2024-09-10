4c17b5216ed083e9580feba3007125a247eb02b7
Denis <dyakimenko@mail.ru>
2024-09-10 16:02:03 +0300

    изменен

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -1,7 +1,5 @@
 package ru.job4j.loop;
 
-//import static org.assertj.core.api.Assertions.*;
-
 public class LoopFor {
     public static void main(String[] args) {
         for (int index = 5; index <= 10; index++) {
