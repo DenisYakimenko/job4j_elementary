ad51464beffd2f15b55976928a52562cc0a06d4c
Denis <dyakimenko@mail.ru>
2024-09-11 15:57:21 +0300

    добавлен класс Fitness, FitnessTest

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+
+        return month;
+    }
+
+    public static void main(String[] args) {
+        int ivanStrength = 90;
+        int nikStrength = 120;
+        int months = calc(ivanStrength, nikStrength);
+        System.out.println("Ивану потребуется " + months + " месяцев, чтобы превзойти Николая по силе тяги.");
+
+    }
+}
