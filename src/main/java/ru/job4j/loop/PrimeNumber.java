745fffe5cf2b345001dffd6c694e4919cf19d8c6
Denis <dyakimenko@mail.ru>
2024-09-13 01:55:01 +0300

    изменен класс PrimeNumber

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,35 +2,28 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
 
-    public static int calc(int finish) {
-        int count = 0;
-        for (int number = 2; number <= finish; number++) {
-            if (isPrime(number)) {
-                count++;
-            }
-        }
-        return count;
+    public static void main(String[] args) {
 
+        System.out.println(check(5));
+        System.out.println(check(4));
     }
 
-    public static boolean isPrime(int number) {
+    public static boolean check(int number) {
+        boolean isPrime = number > 1;
         if (number <= 1) {
             return false;
         }
 
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                return false;
+                isPrime = false;
+                break;
             }
+
         }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        int finish = 11;
-        int result = calc(finish);
-        System.out.println("Количество простых чисел: " + result);
-
+        return isPrime;
     }
 
 }
+
+
--- src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -7,42 +7,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PrimeNumberTest {
 
     @Test
-    void when5Then3() {
-        int finish = 5;
-        int result = PrimeNumber.calc(finish);
-        int expected = 3;
-        assertThat(result).isEqualTo(expected);
+    void when5ThenTrue() {
+        int number = 5;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
     }
 
     @Test
-    void when11Then5() {
-        int finish = 11;
-        int result = PrimeNumber.calc(finish);
-        int expected = 5;
-        assertThat(result).isEqualTo(expected);
+    void when4ThenFalse() {
+        int number = 4;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
     }
 
     @Test
-    void when2Then1() {
-        int finish = 2;
-        int result = PrimeNumber.calc(finish);
-        int expected = 1;
-        assertThat(result).isEqualTo(expected);
+    void when1ThenFalse() {
+        int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
     }
 
     @Test
-    void when25Then9() {
-        int finish = 25;
-        int result = PrimeNumber.calc(finish);
-        int expected = 9;
-        assertThat(result).isEqualTo(expected);
+    void when11ThenTrue() {
+        int number = 11;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
     }
 
     @Test
-    void when49Then15() {
-        int finish = 49;
-        int result = PrimeNumber.calc(finish);
-        int expected = 15;
-        assertThat(result).isEqualTo(expected);
+    void when25ThenFalse() {
+        int number = 25;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when9ThenFalse() {
+        int number = 9;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when49ThenFalse() {
+        int number = 49;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
     }
 }
