f92d43423bc3f3c38cb2aa22a6e006a80c63db46
Denis <dyakimenko@mail.ru>
2024-09-18 20:35:42 +0300

    изменен класс ArrayDefinition

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -14,8 +14,9 @@ public class ArrayDefinition {
         names[1] = "Ivan";
         names[2] = "Dima";
         names[3] = "Oleg";
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
