62158105195c42870effa15c91501414a080450f
Denis <dyakimenko@mail.ru>
2024-09-19 21:13:03 +0300

    change class SwitchArray, SwitchArrayTest [#505230]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,6 +1,13 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int destination) {
+        int temp = array[source];
+        array[source] = array[destination];
+        array[destination] = temp;
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
@@ -11,8 +18,15 @@ public class SwitchArray {
     public static void main(String[] args) {
         int[] numbers = new int[]{1, 2, 3, 4, 5, 6};
         int[] result = swapBorder(numbers);
+        int[] array = {1, 2, 3, 4, 5};
+        swap(array, 2, 3);
         for (int index = 0; index < result.length; index++) {
             System.out.println(result[index]);
+        }
+
+        System.out.println("Массив после замены: ");
+        for (int value : array) {
+            System.out.print(value + " ");
         }
     }
 }
