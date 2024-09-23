eb517b97762104ffed05b977a7b7a6c5ef583ed3
Denis <dyakimenko@mail.ru>
2024-09-23 19:35:35 +0300

    created class ArrayCheck and ArrayCheckTest, task[#505233]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] prefix) {
+        boolean result = true;
+        if (prefix.length > word.length) {
+            return false;
+        }
+
+        for (int i = 0; i < prefix.length; i++) {
+
+            if (word[i] != prefix[i]) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] word = {'h', 'e', 'l', 'l', 'o'};
+        char[] prefix1 = {'h', 'e'};
+        char[] prefix2 = {'h', 'i'};
+
+        System.out.println("Test 1: " + startsWith(word, prefix1));
+        System.out.println("Test 2: " + startsWith(word, prefix2));
+
+    }
+}
