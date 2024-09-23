b3f365b7b66f8dd73bf832561942e1f3a080db80
Denis <dyakimenko@mail.ru>
2024-09-23 23:00:34 +0300

    Обход массива с последнего элемента [#505235]

---
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class RollBackArray {
+    public static int[] rollback(int[] array) {
+        int[] result = new int[array.length];
+        for (int index = 0; index < array.length; index++) {
+            result[index] = array[array.length - index - 1];
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] origin = {1, 2, 3, 4, 5};
+        int[] reversedArray = rollback(origin);
+        for (int index = 0; index < reversedArray.length; index++) {
+            System.out.print(reversedArray[index] + " ");
+        }
+    }
+}
