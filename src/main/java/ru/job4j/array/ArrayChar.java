f205d3f12e1fc74c45ab9705c29faa26b0080a5d
Denis <dyakimenko@mail.ru>
2024-09-25 00:25:15 +0300

    Обход массива с последнего элемента [#505235]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,15 +6,12 @@ public class ArrayChar {
         if (prefix.length > word.length) {
             return false;
         }
-
         for (int i = 0; i < prefix.length; i++) {
-
             if (word[i] != prefix[i]) {
                 result = false;
                 break;
             }
         }
-
         return result;
     }
 
@@ -22,9 +19,7 @@ public class ArrayChar {
         char[] word = {'h', 'e', 'l', 'l', 'o'};
         char[] prefix1 = {'h', 'e'};
         char[] prefix2 = {'h', 'i'};
-
         System.out.println("Test 1: " + startsWith(word, prefix1));
         System.out.println("Test 2: " + startsWith(word, prefix2));
-
     }
 }
