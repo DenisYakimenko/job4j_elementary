4208b567cee749f94600f6c0a6e1955083669791
Denis <dyakimenko@mail.ru>
2024-09-27 16:39:30 +0300

    Двумерный массив. Циклы [#505243]

---
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class MatrixSum {
+    public static int sum(int[][] array) {
+        int result = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                result += array[row][cell];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {
+                {4, 2, 3},
+                {7, 1, 5},
+                {3, 6, 4}
+        };
+        int totalSum = sum(array);
+        System.out.println("Сумма всех элементов массива: " + totalSum);
+    }
+}
