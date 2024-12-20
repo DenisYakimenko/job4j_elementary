9c0a0188921f561ea3d18cbb497bb75451ca2cc6
Denis <dyakimenko@mail.ru>
2024-12-20 22:12:22 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindBySurname(){
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Pavel", "Sidorov", "534111", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("Sidorov");
+        assertThat(persons.get(0).getName()).isEqualTo("Pavel");
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Pavel", "Sidorov", "534111", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("534");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Sidorov");
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Pavel", "Sidorov", "534111", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("Mos");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Sidorov");
+    }
+
+    @Test
+    public void whenFindThenEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Pavel", "Sidorov", "534111", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("Ivanov");
+        assertThat(persons).isEmpty();
+    }
+
+}
