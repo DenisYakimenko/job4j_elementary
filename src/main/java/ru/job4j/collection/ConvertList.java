0bcb76660d36123f9d6ab25af27e1b654d977dff
Denis <dyakimenko@mail.ru>
2024-12-26 22:07:36 +0300

    8. Конвертация листа массивов в один лист Integer [#10037]

---
+++ src/main/java/ru/job4j/collection/ConvertList.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] array : list) {
+            for (int number : array) {
+                result.add(number);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[] {1});
+        list.add(new int[] {2, 3});
+        List<Integer> result = ConvertList.convert(list);
+        System.out.println(result);
+    }
+}
